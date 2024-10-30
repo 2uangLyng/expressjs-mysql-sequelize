81947cfe7623ce3c5e6453e3aa50eb4897785f4f
Võ Quang Linh <21dh111007@st.huflit.edu.vn>
2024-10-30 12:59:56 +0700

    init commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+const db = require('./models');
+
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+db.sequelize.sync({alter: true}).then(() => {
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
+});
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
---
+++ models/User.js
@@ -0,0 +1,63 @@
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define("User", {
+        id: {
+            type: DataTypes.STRING,
+            primaryKey: true,
+            allowNull: false,
+        },
+        firstname: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1, 50],
+            },
+        },
+        lastname: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1, 50],
+            },
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isEmail: true,
+            },
+            unique: true,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [8, 100],
+            },
+        },
+        phone: {
+            type: DataTypes.STRING, // Thay đổi từ NUMERIC sang STRING để kiểm soát độ dài
+            allowNull: false,
+            validate: {
+                len: [10, 11],
+            },
+            unique: true,
+        },
+    }, {
+        timestamps: true,
+        tableName: "user",
+    });
+
+    User.associate = (models) => {
+        User.belongsTo(models.Role, {
+            foreignKey: 'roleId',
+            as: 'role',
+        });
+
+        User.belongsTo(models.Provider, {
+            foreignKey: 'providerId',
+            as: 'provider',
+        });
+    };
+
+    return User;
+};
